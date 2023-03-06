2cea7a59a01eff25a24a0d931188f642ed9ccf3e
galbahary <127109446+galbahary@users.noreply.github.com>
2023-03-06 15:05:14 +0200

    Add files via upload

---
+++ data_structres_practice/cmake-build-debug/CMakeFiles/ex6_galbah.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex6_galbah.dir/test_ex6.cpp.o"
+  "ex6_galbah"
+  "ex6_galbah.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex6_galbah.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
