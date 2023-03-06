06882b87467e7500482a10c1e7f9543ff7d1b25a
galbahary <127109446+galbahary@users.noreply.github.com>
2023-03-06 14:58:03 +0200

    Add files via upload

---
+++ number_identifier/cmake-build-debug/CMakeFiles/ex4_galbah.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/galba/CLionProjects/ex4-galbah/Activation.cpp" "/mnt/c/Users/galba/CLionProjects/ex4-galbah/cmake-build-debug/CMakeFiles/ex4_galbah.dir/Activation.cpp.o"
+  "/mnt/c/Users/galba/CLionProjects/ex4-galbah/Dense.cpp" "/mnt/c/Users/galba/CLionProjects/ex4-galbah/cmake-build-debug/CMakeFiles/ex4_galbah.dir/Dense.cpp.o"
+  "/mnt/c/Users/galba/CLionProjects/ex4-galbah/Matrix.cpp" "/mnt/c/Users/galba/CLionProjects/ex4-galbah/cmake-build-debug/CMakeFiles/ex4_galbah.dir/Matrix.cpp.o"
+  "/mnt/c/Users/galba/CLionProjects/ex4-galbah/MlpNetwork.cpp" "/mnt/c/Users/galba/CLionProjects/ex4-galbah/cmake-build-debug/CMakeFiles/ex4_galbah.dir/MlpNetwork.cpp.o"
+  "/mnt/c/Users/galba/CLionProjects/ex4-galbah/main.cpp" "/mnt/c/Users/galba/CLionProjects/ex4-galbah/cmake-build-debug/CMakeFiles/ex4_galbah.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ number_identifier/cmake-build-debug/CMakeFiles/ex4_galbah.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4_galbah.dir/Activation.cpp.o"
+  "CMakeFiles/ex4_galbah.dir/Dense.cpp.o"
+  "CMakeFiles/ex4_galbah.dir/Matrix.cpp.o"
+  "CMakeFiles/ex4_galbah.dir/MlpNetwork.cpp.o"
+  "CMakeFiles/ex4_galbah.dir/main.cpp.o"
+  "ex4_galbah"
+  "ex4_galbah.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex4_galbah.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
